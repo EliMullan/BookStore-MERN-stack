b44b956764a19275e2164ce3b30e026a93fbfdfe
Eli Mullan <EliMullan21@gmail.com>
2023-09-22 22:47:23 +0100

    Initial commit

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555
+
+export const mongoDBURL = 
+'mongodb+srv://elimullan21:G1DbTE4YCPeVEzOk@books-store-mern.nnmu13g.mongodb.net/books-collection?retryWrites=true&w=majority';
