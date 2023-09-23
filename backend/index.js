10ab410ae53f00fea4d4966c67a56cc1767ba5e6
Eli Mullan <EliMullan21@gmail.com>
2023-09-23 09:56:19 +0100

    create DELETE book route

--- backend/index.js
+++ backend/index.js
@@ -84,7 +84,21 @@ app.put('/books/:id', async (request, response) => {
   }
 })
 
-
+//route for delete a book
+app.delete('/books/:id', async (request, response) => {
+  try {
+    const { id } = request.params;
+    const result = await Book.findByIdAndDelete(id);
+    if(!result) {
+      return response.status(404).json({message: "Book not found"});
+    }
+    return response.status(200).send({message: "Book succesfully deleted"});
+    
+  } catch (error) {
+    console.log(error);
+    return response.status(500).send({message: error.message});
+  }
+})
 
 mongoose
   .connect(mongoDBURL)
